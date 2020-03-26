1b3f1e71bd23a84d828794fef4d9c1f602f0d01b
RusKnyaz <petuhov_k@mail.ru>
2020-03-26 19:51:47 +0700

    Added 'FontSize' option.

--- TextExportApp/Program.cs
+++ TextExportApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
+using System.Text;
 using GrapeCity.ActiveReports;
 using GrapeCity.ActiveReports.Rdl.Persistence;
 using GrapeCity.ActiveReports.Rendering;
@@ -13,18 +15,43 @@ namespace TextExportApp
         {
 	        if (args.Length == 0)
 	        {
-		        Console.WriteLine("Please specify report file name.");
+		        var helpStringBuilder = new StringBuilder();
+		        helpStringBuilder.AppendLine("Usage: TextExportApp.exe <ReportName> [options]");
+		        helpStringBuilder.AppendLine(
+			        "Options: -FontSize=W_H - Specifies font size for measurement in points, like 6_12");
+		        Console.WriteLine(helpStringBuilder.ToString());
 		        return;
 	        }
 
 	        var reportName = args[0];
 
-            var iReport = LoadReport(reportName).BuildReport();
+	        var textSettings = new TxtSettings(){
+		        FontSizePt = new Size(6, 30)
+	        };
+
+	        if (args.Length > 1)
+	        {
+		        try
+		        {
+			        var str = args[1].Split('=')[1].Split('_');
+			        textSettings.FontSizePt.Width = int.Parse(str[0]);
+			        textSettings.FontSizePt.Height = int.Parse(str[1]);
+		        }
+		        catch
+		        {
+			        Console.WriteLine("Invalid options");
+			        return;
+		        }
+	        }
+
+	        var reportDef = LoadReport(reportName);
+
+            var iReport = reportDef.BuildReport();
             
             using(var writer = new StreamWriter(Console.OpenStandardOutput()))
             {
 	            Console.SetOut(writer);
-	            TextRendering.Render(iReport, writer, new TxtSettings());
+	            TextRendering.Render(iReport, writer, textSettings);
             }
         }
 
